392921a06a404815b668f4de0086a396c3d9ae67
Samuel Ventura <samuelventura.contato@gmail.com>
2023-01-13 14:29:05 -0300

    adicionando componente de sidebar, posts e estrutura do básica do css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { Header } from "./components/Header";
+import { Post } from "./Post";
 
 import './global.css'
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
 
 export function App() {
   return (
     <>
       <Header />
-      <h1>Hello World</h1>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Samuel Ventura"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deleniti est cupiditate totam ad ea exercitationem, aliquid iste perferendis autem reiciendis nobis harum velit. Non sapiente cumque iusto ut dolor."
+          />
+          <Post
+            author="Jânio Quadros"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deleniti est cupiditate totam ad ea exercitationem, aliquid iste perferendis autem reiciendis nobis harum velit. Non sapiente cumque iusto ut dolor."
+          />
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props) {
+  return (
+    <>
+      <strong>{props.author}</strong>
+      <h1>{props.content}</h1>
+    </>
+  )
+}
